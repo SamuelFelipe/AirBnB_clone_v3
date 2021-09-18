003275394365e2b7c6a334d395efbc4cf18a1ec3
SamuelFelipe <2354@holbertonschool.com>
2021-09-18 18:59:30 -0500

    Update - files documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 '''
+main file from the api
 '''
 
 from api.v1.views import app_views
 
+
 @app_views.route('/status', strict_slashes=False)
 def status():
+    '''return the api status'''
     return {'status': 'OK'}
