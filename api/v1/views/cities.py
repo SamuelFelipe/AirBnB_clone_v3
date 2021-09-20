673cf1eaf67ad7abe2ca0390e73f88facea8bc66
SamuelFelipe <2354@holbertonschool.com>
2021-09-20 18:28:31 -0500

    Revert "Update - code style"
    This reverts commit d9ad10fd2ed08a71ff7d781b203ab96b5c8b8e43.

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 '''
-Api interactions with City class
+
 '''
 
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
-from models.city import City
 from models.state import State
+from models.city import City
 
 
 @app_views.route('/states/<string:state_id>/cities',
