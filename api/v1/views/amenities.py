03d7f46a5eb7bf564e5bbc673e26ffd7d54975d7
SamuelFelipe <2354@holbertonschool.com>
2021-09-19 19:32:23 -0500

    Fix - bad import

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,7 +7,7 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
-from models.amenities import Amenity
+from models.amenity import Amenity
 
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
