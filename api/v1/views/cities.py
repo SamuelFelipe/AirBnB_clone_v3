4282be27074d10d36101c703323be729d58a0b03
SamuelFelipe <2354@holbertonschool.com>
2021-09-20 16:28:41 -0500

    Update - abort returns

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -53,9 +53,9 @@ def create_city(state_id):
         abort(404)
     info = request.get_json()
     if not info:
-        return 'Not a JSON\n', 400
+        abort(400, description='Not a JSON')
     elif not info.get('name'):
-        return 'Missing name\n', 400
+        abort(400, description='Missing name')
     new = City(**info, state_id=state_id)
     new.save()
     return new.to_dict(), 201
@@ -70,9 +70,9 @@ def update_city(city_id):
         abort(404)
     info = request.get_json()
     if not info:
-        return 'Not a JSON\n', 400
+        abort(400, description='Not a JSON')
     elif not info.get('name'):
-        return 'Missing name\n', 400
+        abort(400, description='Missing name')
     if city:
         info.pop('id', None)
         info.pop('created_at', None)
