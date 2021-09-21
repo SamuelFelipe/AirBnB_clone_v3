d66ac4bdc73e455820e1bebe8d64df7c7c90be5c
SamuelFelipe <2354@holbertonschool.com>
2021-09-21 11:05:02 -0500

    Fix - syntax error on __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,4 +12,4 @@ import api.v1.views.index
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.states import *
-from api.v1.views.user import *
+from api.v1.views.users import *
