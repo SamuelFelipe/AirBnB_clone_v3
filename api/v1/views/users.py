09a8ce9e846b9f78c6b4d87e796ea2b66b42afb0
SamuelFelipe <2354@holbertonschool.com>
2021-09-21 11:00:31 -0500

    Fix - syntax error on api views users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -7,7 +7,7 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
-from models.useres import User
+from models.users import User
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
