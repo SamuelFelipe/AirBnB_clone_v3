32edf6bd589edc972b572d705f979da1ebf463ac
SamuelFelipe <2354@holbertonschool.com>
2021-09-21 10:43:02 -0500

    Upload - users api interactions

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,3 +12,4 @@ import api.v1.views.index
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.states import *
+form api.v1.views.users import *
---
+++ api/v1/views/users.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+'''
+
+'''
+
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.useres import User
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_users():
+    '''list all users'''
+    list_user = [user.to_dict() for user in storage.all(User).values()]
+    return list_user
+
+
+@app_views.route('/users/<string:id>', methods=['GET'], strict_slashes=False)
+def get_user(id):
+    '''get a user by the id'''
+    user = storage.get(User, id)
+    if not user:
+        abort(404)
+    return user.to_dict()
+
+
+@app_views.route('/users/<string:id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_user(id):
+    '''delete a user'''
+    user = storage.get(User, id)
+    if not user:
+        abort(404)
+    user.delete()
+    storage.save()
+    return {}, 200
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    '''create a user'''
+    info = request.get_json()
+    if not info:
+        abort(400, 'Not a JSON')
+    if not info.get('email'):
+        abort(400, 'Missing email')
+    elif not info.get('password'):
+        abort(400, 'Missing password')
+    new = User(**info)
+    storage.save()
+    return new.to_dict(), 201
+
+
+@app_views.route('/users/<string:id>', methods=['PUT'], strict_slashes=False)
+def update_user(id):
+    '''Update a user'''
+    info = request.get_json()
+    if not info:
+        abort(400, 'Not a JSON')
+    user = storage.get(User, id)
+    if not user:
+        abort(404)
+    info.pop('id', None)
+    info.pop('email', None)
+    info.pop('created_at', None)
+    info.pop('updated_at', None)
+    for key, val in info.items():
+        setattr(user, key, val)
+    user.save()
+    return user.to_dict(), 200
